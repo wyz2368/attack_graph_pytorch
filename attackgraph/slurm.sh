315b6cfce567629ca2b13f901bf1af0629a83a48
wangyzh <wangyzh@umich.edu>
2020-05-26 00:32:01 -0400

    "remove extra data"

--- attackgraph/slurm.sh
+++ attackgraph/slurm.sh
@@ -5,8 +5,8 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem-per-cpu=7g
-#SBATCH --time=02-00:00:00
+#SBATCH --mem-per-cpu=14g
+#SBATCH --time=03-00:00:00
 #SBATCH --account=wellman1
 #SBATCH --partition=standard
 
